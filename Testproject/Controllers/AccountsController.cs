824b5d1d175083ef5a4b696dde6feb622ba8b801
AmjedAgabani <amjedagabani@gmail.com>
2018-01-04 21:28:41 +0000

    Added login

---
+++ Testproject/Controllers/AccountsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Testproject.Models;
+
+namespace Testproject.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel viewModel)
+        {
+            if (viewModel.Email == "a@b.c" && viewModel.Password == "pass")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            
+            return View();
+        }
+    }
+}
--- Testproject/Controllers/HomeController.cs
+++ Testproject/Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace Testproject.Controllers
             return View();
         }
 
+        public IActionResult Help()
+        {
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
